f03df0476d1b389413b677c99436613d5302ef2b
Jan Steikowski <j.steikowski@aol.com>
2023-05-28 11:30:27 +0700

    Finish Header Style, add new font

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -7,12 +7,15 @@
 function loadHeader() {
   // create Header Element
   const header = document.createElement('header');
-  header.classList = ['bg-green-400 flex flex-col justify-center items-center'];
+  // add styling to header
+  header.classList = ['bg-black flex flex-col justify-center items-center'];
   // create HeaderContent
   const heading = document.createElement('h1');
   heading.innerText = 'Mini-Do';
+  heading.classList = ['text-white text-4xl'];
   const subHeading = document.createElement('h2');
   subHeading.innerText = 'The minimalistic To-Do-List.';
+  subHeading.classList = ['text-white text-xl mb-1'];
   // add headerContent to HeaderElement
   header.appendChild(heading);
   header.appendChild(subHeading);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html", "./src/*.js"],
+  content: ['./dist/*.html', './src/*.js'],
   theme: {
     extend: {
       fontFamily: {
-        body: ["Gaegu"],
+        body: ['Quicksand'],
       },
     },
   },
