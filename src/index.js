7fbf0b426ccd995cbe9897e0262201e24789cc19
Jan Steikowski <j.steikowski@aol.com>
2023-05-28 12:42:12 +0700

    Add Todo Class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import {loadPage} from './pageLoad';
+import {logTodo1} from './todo';
 
 const body = document.getElementById('body');
 body.appendChild(loadPage());
+logTodo1();
---
+++ src/todo.js
@@ -0,0 +1,69 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+class Todo {
+  constructor(title, description, dueDate, priority, checklist = '', notes = '') {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checklist = checklist;
+    this.notes = notes;
+  }
+  getTitle = () => {
+    return this.title;
+  };
+
+  getDescription = () => {
+    return this.description;
+  };
+
+  getDueDate = () => {
+    return this.dueDate;
+  };
+
+  getPriority = () => {
+    return this.priority;
+  };
+
+  getChecklist = () => {
+    return this.checklist;
+  };
+
+  getNotes = () => {
+    return this.notes;
+  };
+
+  setTitle = (title) => {
+    this.title = title;
+  };
+
+  setDescription = (description) => {
+    this.description = description;
+  };
+
+  setDueDate = (dueDate) => {
+    this.dueDate = dueDate;
+  };
+
+  setPriority = (priority) => {
+    this.priority = priority;
+  };
+
+  setChecklist = (checkList) => {
+    this.checklist = checkList;
+  };
+
+  setNotes = (notes) => {
+    this.notes = notes;
+  };
+}
+
+const todo1 = new Todo('Todo erstellen', 'Hier in dem Skript ein Todo erstellen', 'today', 'high');
+
+const logTodo1 = () => {
+  console.log(todo1);
+};
+
+export {Todo, logTodo1};
+
+
