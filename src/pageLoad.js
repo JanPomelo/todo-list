aafac98bce4637b2aa9e5bb419dd4194a308308f
Jan Steikowski <j.steikowski@aol.com>
2023-05-29 13:21:09 +0700

    Add the main content background

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 const loadHeaderMainHeading = () => {
@@ -25,9 +26,27 @@ const loadHeader = () => {
   return header;
 };
 
+
+const loadTable = () => {
+  const table = document.createElement('table');
+  const header = document.createElement('th');
+  table.appendChild(header);
+  return table;
+};
+
+const loadToDoDiv = () => {
+  const todoDiv = document.createElement('div');
+  todoDiv.classList = [
+    'h-full w-full sm:rounded-3xl sm:border-4 border-black sm:h-4/5 sm:w-full bg-white shadow-gray-950 sm:shadow-xl sm:shadow-gray-950',
+  ];
+  todoDiv.appendChild(loadTable());
+  return todoDiv;
+};
+
 const loadMain = () => {
   const main = document.createElement('main');
-  main.classList = ['flex-grow'];
+  main.classList = ['flex-grow bg-grey-100 flex justify-center items-center sm:pl-5 sm:pr-5 xl:pr-20 xl:pl-20'];
+  main.appendChild(loadToDoDiv());
   return main;
 };
 
