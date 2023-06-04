6789c4a02ec4b75fa2ee7f1d9c5b3a884dde0ff5
Jan Steikowski <j.steikowski@aol.com>
2023-06-04 10:04:16 +0700

    Add stylings to delete PopUp

--- src/domManips.js
+++ src/domManips.js
@@ -1,21 +1,24 @@
+/* eslint-disable max-len */
 'use strict';
 
 const reallySure = () => {
   const content = document.getElementById('content');
   const popUp = document.createElement('div');
-  popUp.classList = ['absolute grid grid-cols-2 gap-y-2'];
+  popUp.classList = ['absolute grid grid-cols-2 gap-y-2 border-2 border-black rounded-xl p-2 max-w-sm shadow-xl'];
   popUp.id = 'popUpDelete';
   const popUpHeadline = document.createElement('h3');
   popUpHeadline.innerText = 'Are you sure?';
-  popUpHeadline.classList = ['col-span-2'];
+  popUpHeadline.classList = ['col-span-2 text-2xl font-bold'];
   const popUpText = document.createElement('p');
   // eslint-disable-next-line max-len
   popUpText.innerText = 'You are about to delete this To-Do from your current Project. You can\'t restore it. Are you sure you want to do this?';
   popUpText.classList = ['col-span-2'];
   const popUpYes = document.createElement('button');
   popUpYes.innerHTML = 'Delete';
+  popUpYes.classList = ['border-2 border-black rounded-lg w-4/5 justify-self-center bg-black text-white active:shadow-md active:shadow-indigo-500'];
   const popUpNo = document.createElement('button');
   popUpNo.innerHTML = 'Keep';
+  popUpNo.classList = popUpYes.classList;
   popUp.appendChild(popUpHeadline);
   popUp.appendChild(popUpText);
   popUp.appendChild(popUpYes);
